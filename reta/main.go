96f8dd86bb9642ae2e10ef0936097d364a491b04
Benjamim <benjamim.francisco@hotmail.com>
2020-07-29 12:34:46 -0300

    iniciando com pacotes e visibilidade

---
+++ reta/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//inicializando chamando funcoes dentro do pacote
+	p1 := Ponto{2.0, 2.0}
+	p2 := Ponto{2.0, 4.0}
+	fmt.Println(catetos(p1, p2))
+}
+
+//rodando em windows : go run main.go reta.go
+//linux go run *.go
---
+++ reta/reta.go
@@ -0,0 +1,28 @@
+package main
+
+import "math"
+
+//Metodos, variaveis, funcoes e etc com letra maiuscula são de visibilidade publica(dentro e fora do pacote)
+// logo letras minusculas indicam que sao privados dentro do pacote
+// ponto no inicio do nome = publico/ ponto e pontoUnderline privado
+
+// por ser publico deve possuir comentario
+type Ponto struct {
+	x float64
+	y float64
+}
+
+//calcular catetos
+func catetos(a, b Ponto) (cx, cy float64) {
+	//obetendo valores absolutos com pacote math
+	cx = math.Abs(b.x - a.x)
+	cy = math.Abs(b.y - a.y)
+	return //return limpo/retorno nomeado
+}
+
+//funcao publica p/ calcular distancia
+func Distancia(a, b Ponto) float64 {
+	cx, cy := catetos(a, b)
+	//retornando a raiz quadrada da soma dos quadrados dos catetos
+	return math.Sqrt(math.Pow(cx, 2) + math.Pow(cy, 2))
+}
